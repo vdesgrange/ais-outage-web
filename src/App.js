124a2f893a231e76d86b21e9359c0e30edc61fd9
Viviane Desgrange <viviane.desgrange@gmail.com>
2021-10-25 01:31:37 +0200

    Test sample.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
+import react from 'react';
+import axios from 'axios';
+import { MapContainer, TileLayer,  GeoJSON } from 'react-leaflet';
 import './App.css';
-import { MapContainer, TileLayer, Marker } from 'react-leaflet';
-
+import dataUrl from './test.geojson'
+ 
 function App() {
+  const [data, setData] = react.useState();
+  react.useEffect(async () => { 
+    const res = await axios(dataUrl);
+    setData(res.data);
+  }, []);
+  const style = (feature) => {
+      return {
+        weight: 3,
+        opacity: 1,
+        color: 'red',
+        dashArray: '3',
+        fillOpacity: 0.0
+      };
+    }
+  console.log(data);
+
   return (
-      <MapContainer id="mapid" center={[51.505, -0.09]} zoom={8} scrollWheelZoom={true}>
+      <MapContainer id="mapid" center={[29.885237, 122.16163]} zoom={8} scrollWheelZoom={true}>
         <TileLayer
           attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
@@ -11,8 +30,7 @@ function App() {
         <TileLayer
           url="https://tiles.openseamap.org/seamark/{z}/{x}/{y}.png"
         />
-        <Marker position={[51.505, -0.09]}>
-        </Marker>
+        { data && <GeoJSON data={data} style={style} />}
       </MapContainer>
   );
 }
