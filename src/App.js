d8b1a3a4562d197856855685972957d7b0310228
Viviane Desgrange <viviane.desgrange@gmail.com>
2021-10-28 17:30:46 +0200

    Add time slider.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,35 @@
 import react from 'react';
 import axios from 'axios';
 import { MapContainer, TileLayer,  GeoJSON } from 'react-leaflet';
+import TimeRange from 'react-timeline-range-slider'
+import moment from 'moment';
 import './App.css';
 import dataUrl from './test2.geojson'
- 
-function App() {
-  const [data, setData] = react.useState();
-  react.useEffect(async () => { 
-    const res = await axios(dataUrl);
-    setData(res.data);
-  }, []);
-  const style = (feature) => {
+
+const startTime = moment("2016-01-01 00:00:00");
+const endTime = moment("2017-12-31 23:59:59");
+
+class App extends react.Component {
+  state = {
+      error: false,
+      selectedInterval: [startTime, endTime],
+      data: undefined,
+  }
+
+  async componentDidMount() {
+    const {data} = await axios(dataUrl);
+    this.setState({data});
+    //react.useEffect(async () => { 
+    //}, []);
+  }
+
+  errorHandler = ({ error }) => this.setState({ error })
+
+  onChangeCallback = selectedInterval => this.setState({ selectedInterval })
+
+  formatTick = ms => moment(ms).format("MMM 'YY");
+
+  style = (feature) => {
       return {
         weight: 3,
         opacity: 1,
@@ -19,20 +38,43 @@ function App() {
         fillOpacity: 0.0
       };
     }
-  console.log(data);
 
-  return (
-      <MapContainer id="mapid" center={[29.885237, 122.16163]} zoom={8} scrollWheelZoom={true}>
-        <TileLayer
-          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-        <TileLayer
-          url="https://tiles.openseamap.org/seamark/{z}/{x}/{y}.png"
-        />
-        { data && <GeoJSON data={data} style={style} />}
-      </MapContainer>
-  );
+
+  render() {
+    const {data, selectedInterval} = this.state;
+
+    return (
+      <div className="ais-wrapper">
+        <MapContainer id="mapid" center={[52.377956, 4.897070]} zoom={9} scrollWheelZoom={true}>
+          <TileLayer
+            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+          <TileLayer
+            url="https://tiles.openseamap.org/seamark/{z}/{x}/{y}.png"
+          />
+          { data && <GeoJSON data={data} style={this.style} />}
+        </MapContainer>
+
+        <div className='time-range-section'>
+          <div className='time-range-details'>
+      <div className='from'><b>From</b> {moment(selectedInterval[0]).format("D MMMM 'YY HH:mm")}</div>
+      <div className='to'><b>To</b> {moment(selectedInterval[1]).format("D MMMM 'YY HH:mm")}</div>
+          </div>
+          <TimeRange
+            //sliderRailClassName="time-slider"
+            containerClassName="time-container"
+            timelineInterval={[startTime, endTime]}
+            selectedInterval={selectedInterval}
+            onUpdateCallback={this.errorHandler}
+            onChangeCallback={this.onChangeCallback}
+            formatTick={this.formatTick}
+            ticksNumber={24}
+          />
+          </div>
+      </div>
+    );
+  }
 }
 
 export default App;
